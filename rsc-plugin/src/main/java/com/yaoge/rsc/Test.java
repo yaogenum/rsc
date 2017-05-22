76def6866294101c262b2338416e6e96218b378e
yaogenum <yaogemail@gmail.com>
2017-05-22 21:48:14 +0800

    update dir structure

---
+++ rsc-plugin/src/main/java/com/yaoge/rsc/Test.java
@@ -0,0 +1,7 @@
+package com.yaoge.rsc;
+
+/**
+ * Created by yaoge on 16/6/26.
+ */
+public class Test {
+}
---
+++ rsc-plugin/src/test/java/com/yaoge/rsc/Test2.java
@@ -0,0 +1,11 @@
+package com.yaoge.rsc;
+
+/**
+ * Created by yaoge on 16/6/26.
+ */
+public class Test2 {
+
+        public static void main(String[] args) {
+            System.out.println("s");
+        }
+}
